13ee4e08b61f901646cf9fffa580502b69a5089c
Moderator <doananhdung.work@gmail.com>
2024-09-23 19:58:36 +0700

    cartegory

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//--------------------Menu Slidebar cartegory--------------------------
+const itemsliderbar = document.querySelectorAll(".cartegory-left-li");
+itemsliderbar.forEach(function (menu, index) {
+  menu.addEventListener("click", function () {
+    menu.classList.toggle("block");
+  });
+});
