446159a08fc1de9848e8f1ea6e5a2e3f53605762
Moderator <doananhdung.work@gmail.com>
2024-09-24 13:40:21 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,59 @@
+//--------------------Home slide --------------------------
+
+const imPosition = document.querySelectorAll("#slider img");
+const isContainer = document.querySelector(".aspect-ratio-169");
+const dotsItem = document.querySelectorAll(".dot");
+let index = 0;
+
+imPosition.forEach((img, index) => {
+  img.style.left = `${index * 100}%`;
+});
+addEventListenerToDots();
+
+function imgSlide() {
+  index++;
+  if (index > imPosition.length - 1) {
+    index = 0;
+  }
+
+  isContainer.style.left = "-" + index * 100 + "%";
+
+  for (let i = 0; i < imPosition.length; i++) {
+    if (i === index) {
+      document.querySelectorAll(".dot")[i].classList.add("active");
+    } else {
+      document.querySelectorAll(".dot")[i].classList.remove("active");
+    }
+  }
+}
+
+function slideTo(index) {
+  isContainer.style.left = "-" + index * 100 + "%";
+}
+function addEventListenerToDots() {
+  dotsItem.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      slideTo(index);
+    });
+  });
+}
+setInterval(imgSlide, 2000);
+
+//-------------------- scroll--------------------------
+window.addEventListener("scroll", function () {
+  x = window.scrollY;
+  if (x > 0) {
+    document.querySelector("header").style.backgroundColor = "white";
+    document.querySelector("header").style.boxShadow =
+      "0 0 10px rgba(0, 0, 0, 0.1)";
+    document.querySelector("header").style.transition = "0.5s";
+  } else {
+    document.querySelector("header").style.backgroundColor =
+      "rgba(255, 255, 255, 0.3)";
+    document.querySelector("header").style.boxShadow = "none";
+  }
+});
+
 //--------------------Menu Slidebar cartegory--------------------------
 const itemsliderbar = document.querySelectorAll(".cartegory-left-li");
 itemsliderbar.forEach(function (menu, index) {
