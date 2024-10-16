ba0fe4460b6ee478f3d0e388b8fb2b12e50ce7d0
Nhi <nhilam150@gmail.com>
2024-10-16 16:36:53 +0700

    done-info

---
+++ js/data/data-news.js
@@ -0,0 +1,200 @@
+const newinfo = [
+    {
+        img: './img/collection/rsz_1Anh_chInh-e1728288339241-1536x826.jpg',
+        time: '02/10/2024',
+        title: 'ĐỊA CHỈ SHOP QUẦN ÂU NAM ĐẸP, UY TÍN, GIÁ HỢP LÝ',
+        content: 'Bạn đang tìm kiếm một chiếc quần âu nam vừa vặn, chất lượng cao mà giá cả lại phải chăng? Việc lựa chọn một địa chỉ uy tín để mua quần âu nam là điều vô cùng quan trọng. Bài viết này sẽ giới thiệu đến bạn các mẫu quần âu nam đẹp, chất lượng, giá cả hợp lý và địa chỉ mua quần âu nam tại Hà Nội uy tín, thêm vào đó là những thông tin hữu ích về các loại vải, kiểu dáng và cách chọn size quần âu phù hợp.'
+    },
+    {
+        img: './img/collection/STDTK421-APTK465-QJDTK309-STDTK421-APTTK423-QJDTK334-2-Medium-e1728017423347.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bạn đang tìm kiếm những cách phối đồ đơn giản nhưng vẫn thật phong cách với quần jean và áo sơ mi? Cặp đôi kinh điển này chưa bao giờ lỗi mốt và luôn mang đến vẻ ngoài lịch lãm, trẻ trung cho phái mạnh. 360 giới thiệu đến bạn 3 cách phối quần jean nam với áo sơ mi nam cực kỳ đa năng, phù hợp với nhiều hoàn cảnh khác nhau.'
+    },
+    {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/sơ mi trắng.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/set-do-3-1536x1024.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/AKGTK501-ANTHM519-QGKTK410-4-e1728632906520.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/STDTK506-QATTK408-1-STDTK501-QATTK408-1-Custom-e1726126950459.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/rsz_vuOng_1-e1726731795856.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/ADLTK401-ALETK402-QKLTK402-AKHTK406-STDTK426-APTTK403-QJDTK418-AKHTK407-ANMTK403-QJDTK403-5-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    }, {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    },
+    {
+        img: './img/collection/POTTK442-QGNTK407-AHHTK403-QGNTK407-4-Custom.jpg',
+        time: '02/10/2024',
+        title: 'BỘ NỈ NAM CAO CẤP PHONG CÁCH THỂ THAO CHẤT LƯỢNG GIÁ TỐT',
+        content: 'Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai.Bộ đồ nỉ nam không còn đơn thuần là trang phục thể thao, mà đã trở thành một item thời trang không thể thiếu trong tủ đồ của mọi chàng trai. Với sự đa dạng về kiểu dáng,'
+    }]
+    ;
+const city = [{
+    id: 1,
+    city: 'Hà Nội'
+},
+{
+    id: 2,
+    city: 'Hải Phòng'
+},
+];
+const district = [{
+    idcity: 2,
+    iddistrict: 1,
+    district: 'Lê Chân'
+},
+{
+    idcity: 1,
+    iddistrict: 2,
+    district: 'Cầu Giấy'
+},
+{
+    idcity: 1,
+    iddistrict: 3,
+    district: 'Đống Đa'
+},
+{
+    idcity: 1,
+    iddistrict: 4,
+    district: 'Hà Đông'
+}
+    , {
+    idcity: 1,
+    iddistrict: 5,
+    district: 'Hai Bà Trưng'
+},
+{
+    idcity: 1,
+    iddistrict: 6,
+    district: 'Nam Từ Liêm'
+}
+]
+const branch = [{
+    idcity: 1,
+    iddistrict: 2,
+    idbranch: 1,
+    address: '11 Dương Quảng Hàm, Quận Cầu Giấy, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.975604627305!2d105.79863879999999!3d21.0336621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab473985771f%3A0xd41e67c2aece0866!2zMTEgRMawxqFuZyBRdeG6o25nIEjDoG0sIFF1YW4gSG9hLCBD4bqndSBHaeG6pXksIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727966648522!5m2!1svi!2s'
+}, {
+    idcity: 1,
+    iddistrict: 2,
+    idbranch: 2,
+    address: '346 Cầu Giấy, Quận Cầu Giấy, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.9268630175943!2d105.7926557!3d21.035612200000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab481974afdb%3A0x505c09b2a5467dc1!2zMzQ2IMSQLiBD4bqndSBHaeG6pXksIEThu4tjaCBW4buNbmcsIEPhuqd1IEdp4bqleSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1727966913200!5m2!1svi!2s'
+
+},
+{
+    idcity: 1,
+    iddistrict: 2,
+    idbranch: 3,
+    address: '116 Hồ Tùng Mậu,Quận Cầu Giấy, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.892148143233!2d105.77677399999999!3d21.037001000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b5fae74879%3A0x900d68fef39908b6!2zMTE2IMSQLiBI4buTIFTDuW5nIE3huq11LCBNYWkgROG7i2NoLCBD4bqndSBHaeG6pXksIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727966987296!5m2!1svi!2s'
+
+},
+{
+    idcity: 1,
+    iddistrict: 2,
+    idbranch: 4,
+    address: '20 Dương Quảng Hàm, Quận Cầu Giấy, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.9812155966883!2d105.79860959999999!3d21.0334376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab473b5be93d%3A0x371718ae4b86d571!2zMjAgRMawxqFuZyBRdeG6o25nIEjDoG0sIFF1YW4gSG9hLCBD4bqndSBHaeG6pXksIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727967106114!5m2!1svi!2s'
+
+}, {
+    idcity: 2,
+    iddistrict: 1,
+    idbranch: 5,
+    address: '272 Tô Hiệu, Quận Lê Chân, Hải Phòng',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.534673063402!2d106.6766092!3d20.850488800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7a89e077cc3d%3A0xf39bc90ce7e6e1e6!2zMjcyIFAuIFTDtCBIaeG7h3UsIEjhu5MgTmFtLCBMw6ogQ2jDom4sIEjhuqNpIFBow7JuZywgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727966697323!5m2!1svi!2s'
+}, {
+    idcity: 1,
+    iddistrict: 6,
+    idbranch: 6,
+    address: '29 Phố Nhổn, Quận Nam Từ Liêm, Hà Nội',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.4661955356005!2d105.73265830000001!3d21.0540345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134545784c1c041%3A0x7ae6c4995b6656a6!2zMjkgUC4gTmjhu5VuLCBYdcOibiBQaMawxqFuZywgSMOgIE7hu5lpLCAxMDAwMDAsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1727966795480!5m2!1svi!2s'
+}, {
+    idcity: 1,
+    iddistrict: 4,
+    idbranch: 7,
+    address: '24 Trần Phú, Quận Hà Đông, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.2603603874395!2d105.7892971!3d20.982198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acce9276f92d%3A0x41d976b1173e2f25!2zTmcuIDI0IMSQLiBUcuG6p24gUGjDuiwgSMOgIMSQw7RuZywgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1727966880859!5m2!1svi!2s'
+}, {
+    idcity: 1,
+    iddistrict: 6,
+    idbranch: 8,
+    address: '298 Nguyễn Trãi, Quận Nam Từ Liêm, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.143295893496!2d105.79633439999999!3d20.986892299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acc7aea9c0cd%3A0x779db1d0b07d54fb!2zMjk4IMSQLiBOZ3V54buFbiBUcsOjaSwgUC4gVsSDbiBRdcOhbiwgTmFtIFThu6sgTGnDqm0sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727966955563!5m2!1svi!2s'
+}, {
+    idcity: 1,
+    iddistrict: 3,
+    idbranch: 9,
+    address: '27 Chùa Bộc, Quận Đống Đa, Hà Nội',
+    hotline: '0975056360',
+    mapbranch: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.6509715351885!2d105.830007!3d21.006623599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac7fcceee60d%3A0x2605d3303820e40e!2zMjcgUC4gQ2jDuWEgQuG7mWMsIEtpbSBMacOqbiwgxJDhu5FuZyDEkGEsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727967019958!5m2!1svi!2s'
+}
+]
---
+++ js/info.js
@@ -0,0 +1,132 @@
+console.log('test');
+Show();
+buttonsearchbranch();
+show_Map();
+function Show() {
+  let valueoutput = '<option value="0">Chọn tỉnh, thành phố</option>';
+  city.forEach((valuecity) => {
+    valueoutput += ` <option value="${valuecity.id}" class="js-city">${valuecity.city}</option>`;
+  });
+  document.querySelector('.option-city').innerHTML = valueoutput;
+  let valueshow = findbranch();
+  document.querySelector('.js-find-branch').innerHTML = `Tìm thấy ${valueshow.count} cửa hàng`;
+  document.querySelector('.js-find-branch-add-branch').innerHTML = `${valueshow.outputHTML}`;
+
+}
+function selectdistrict(event) {
+  value = event.target.value;
+  let valueoutput = '<option value="0">Chọn quận, huyện</option>';
+  district.forEach((valuedistrict) => {
+    if (valuedistrict.idcity == value) {
+      valueoutput += `<option value="${valuedistrict.iddistrict}">${valuedistrict.district}</option>`;
+    }
+  });
+  console.log(valueoutput);
+  document.querySelector('.js-district-add').innerHTML = valueoutput;
+
+}
+function findbranch() {
+  let count = 0;
+  let output = {
+    count: 0,
+    outputHTML: ''
+  };
+
+  valuecity = document.getElementById('option-city-select').value;
+  valuedistrict = document.getElementById('option-district-select').value;
+  if (valuecity == 0) {
+    branch.forEach((value) => {
+      output.count++;
+      output.outputHTML += ` <div class="find-branch-right-row">
+                <div class="find-branch-right-box-input">
+                  <input type="radio" id="${value.idbranch}" name="name-inputmap"  class="input-map-js">
+                  <label for=""></label>
+                </div>
+                <div class="find-branch-right-box-right">
+                  <p class="find-branch-right-address-strong">${value.address}</p>
+                  <p class="find-branch-right-address">${value.address}</p>
+                  <p class="find-branch-right-hotline">${value.hotline}</p>
+                </div>
+
+              </div>`;
+      // branchoutput += `test +${count}`;
+      // console.log('hi');
+    });
+  }
+  else if (valuedistrict == 0) {
+    count = 0;
+    branch.forEach((value) => {
+      if (value.idcity == valuecity) {
+        output.count++;
+        output.outputHTML += ` <div class="find-branch-right-row">
+                <div class="find-branch-right-box-input">
+                <input type="radio" id="${value.idbranch}" name="name-inputmap"  class="input-map-js">
+                  <label for=""></label>
+                </div>
+                <div class="find-branch-right-box-right">
+                  <p class="find-branch-right-address-strong">${value.address}</p>
+                  <p class="find-branch-right-address">${value.address}</p>
+                  <p class="find-branch-right-hotline">${value.hotline}</p>
+                </div>
+
+              </div>`;
+      }
+
+    });
+  }
+
+
+  else {
+    branch.forEach((value) => {
+      if ((value.idcity == valuecity) && (value.iddistrict == valuedistrict)) {
+        // console.log(`value.idcity=${value.idcity} valuecity=${valuecity} value.iddistrict=${value.iddistrict} valuedistrict=${valuedistrict}`)
+        // console.log('test địa chỉ');
+        output.count++;
+        output.outputHTML += ` <div class="find-branch-right-row">
+                <div class="find-branch-right-box-input">
+                  <input type="radio" id="${value.idbranch}" name="name-inputmap" class="input-map-js">
+                  <label for="" id="${value.idbranch}"></label>
+                </div>
+                <div class="find-branch-right-box-right">
+                  <p class="find-branch-right-address-strong">${value.address}</p>
+                  <p class="find-branch-right-address">${value.address}</p>
+                  <p class="find-branch-right-hotline">${value.hotline}</p>
+                </div>
+
+              </div>`;
+      }
+    });
+  }
+
+  return output;
+}
+function buttonsearchbranch() {
+  let count = 0;
+  let branchoutput = '';
+  document.querySelector('.js-select-button-search').addEventListener('click', () => {
+    console.log('test click');
+    valueshow = findbranch();
+    console.log(valueshow.outputHTML);
+    document.querySelector('.js-find-branch').innerHTML = `Tìm thấy ${valueshow.count} cửa hàng`;
+    document.querySelector('.js-find-branch-add-branch').innerHTML = `${valueshow.outputHTML}`;
+
+  });
+
+}
+function show_Map() {
+  console.log("hihi");
+  document.querySelectorAll('.input-map-js').forEach((value) => {
+    value.addEventListener('click', () => {
+      console.log(value.id);
+      branch.forEach((valuebranch) => {
+        if (valuebranch.idbranch == value.id) {
+          document.querySelector('.js-find-branch-add-map').innerHTML = `            <iframe
+              src="${valuebranch.mapbranch}"
+              allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+        }
+      });
+    });
+  });
+
+
+}
