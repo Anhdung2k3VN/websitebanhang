7f5a2534817a4273b0cbf1ecb2a8dc2c9ba53f42
Nhi <nhilam150@gmail.com>
2024-11-01 09:32:23 +0700

    updatesm

--- js/validator.js
+++ js/validator.js
@@ -24,6 +24,7 @@ function Validator(option) {
             inputElement.parentElement.querySelector('.invalid-message').innerHTML = '';
             inputElement.parentElement.classList.remove('invalid');
         }
+        return input;
 
     }
     let formElement = document.querySelector(option.form);
@@ -31,18 +32,36 @@ function Validator(option) {
     if (formElement) {
 
         //Lấy element của form cần validate
-
+        // console.log(formElement);
 
         formElement.onsubmit = function (e) {
-            alert("Hà Thanh Dũng");
+            e.preventDefault();
+            let isFormValid = true;
 
-            // e.preventDefault();
-            // option.rules.forEach(function (rule) {
-            //     let inputElement = formElement.querySelector(rule.selector);
-            //     console.log(inputElement);
-            //     validate(inputElement, rule);
 
-            // });
+            option.rules.forEach(function (rule) {
+                let inputElement = formElement.querySelector(rule.selector);
+                let isValid = validate(inputElement, rule);
+                if (isValid && isFormValid) {
+                    isFormValid = false;
+                }
+            });
+
+            if (isFormValid) {
+                if (typeof option.onSubmit === 'function') {
+                    let enableInputs = formElement.querySelectorAll('[name]:not([disabled])');
+                    let formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        return (values[input.name] = input.value) && values;
+
+                    }, {});
+                    option.onSubmit(formValues);
+                    // console.log(enableInputs);
+                }
+
+
+            }
+
+
 
         }
         option.rules.forEach(function (rule) {
